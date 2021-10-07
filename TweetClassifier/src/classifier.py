178817ee885992879bd0e0b9d4711d0c1835bd5f
jboothby <josephboothby37@gmail.com>
2021-10-06 19:22:04 -0700

    Got api working

--- TweetClassifier/src/classifier.py
+++ TweetClassifier/src/classifier.py
@@ -1,9 +1,14 @@
 import yaml
+import json
+from twython import Twython
 
-with open('config/key.yaml') as file:
+with open('../config/key.yaml') as file:
     api_key = yaml.safe_load(file)
 
-print(api_key)
+twitter = Twython(api_key['key'], api_key['secret'], oauth_version=2)
+ACCESS_TOKEN = twitter.obtain_access_token()
 
+twitter = Twython(api_key['key'], access_token=ACCESS_TOKEN)
 
-
+with open('results.json', 'w') as out:
+    json.dump(twitter.search(q='to:EpicGames'), out, indent=2)
