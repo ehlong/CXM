b75dde96949e32082ff0a05a20f4e1466f57f86d
Taylor <taylor.c.vo@gmail.com>
2022-03-08 22:16:48 -0800

    Now loads SVGs as img tags, loads with any amount of tweets

--- UI/my-electron-app/src/classification.js
+++ UI/my-electron-app/src/classification.js
@@ -8,6 +8,33 @@ function showPage() {
     document.getElementById("loader-container").style.display = "none";
 }
 
+document.addEventListener("DOMContentLoaded", function() {
+    
+    let links = ["positive", "bugs", "security", "store", "wants", "junk"];
+
+    for (var link of links) { 
+        var tweetbox = document.getElementsByClassName(link + "-link"); 
+        let coloredImg = document.createElement("img"); 
+        let grayImg = document.createElement("img"); 
+        coloredImg.setAttribute("class", link + "-color"); 
+        coloredImg.setAttribute("alt", "check"); 
+        coloredImg.setAttribute("title", link.toUpperCase()); 
+        coloredImg.src = "../img/svgs/" + link + "-color.svg"; 
+        
+        grayImg.setAttribute("class", link + "-gray"); 
+        grayImg.setAttribute("alt", "check"); 
+        grayImg.setAttribute("title", link.toUpperCase()); 
+        grayImg.src = "../img/svgs/" + link + "-gray.svg"; 
+        for(var i = 0; i < tweetbox.length ; i++) {
+            tweetbox[i].innerHTML =   
+            "<img class=\"" + link + "-gray\" src=\"../img/svgs/" + link + "-gray.svg\" alt=\"uncheck\" title=\" " + link.toUpperCase() + "\"/>" + "\n" +   
+            "<img class=\"" + link + "-color\" src=\"../img/svgs/" + link + "-color.svg\" alt=\"check\" title=\"" + link.toUpperCase() + "\"/>";
+        
+        }
+    }
+});
+
+
 function get_tweet_callback(data) {
   let element;
   for(let j = 1; j<10; j++){
