6bb1fe58484d433fd46ddae3241439a0cef9a4dc
Taylor <taylor.c.vo@gmail.com>
2022-03-14 09:23:17 -0700

    error with appending html elements, may have to use innerHTML()

--- UI/my-electron-app/src/loadTagsUnclassified.js
+++ UI/my-electron-app/src/loadTagsUnclassified.js
@@ -1,12 +1,12 @@
 
-
 document.addEventListener("DOMContentLoaded", function() {
+    loadTweetBoxes(9);
     loadImgs();
+    
 });
 
-
+let links = ["positive", "bugs", "security", "store", "wants", "junk"];
 function loadImgs() { 
-    let links = ["positive", "bugs", "security", "store", "wants", "junk"];
 
     for (var link of links) { 
         var tweetbox = document.getElementsByClassName(link + "-link"); 
@@ -32,5 +32,49 @@ function loadImgs() {
 
 
 function loadTweetBoxes (n) { 
+    var t_box = document.getElementsByClassName("Tweetbox"); 
+
+    for(var i =1; i <= n; i++) { 
+        var tweet = document.createElement("div"); 
+        tweet.setAttribute("id", "Tweet" + i); 
+        tweet.setAttribute("class", "tweet"); 
+        tweet.innerHTML = 
+        "<h2>TweetID </h2>" + '\n' + 
+        "<p> Tweet Stuffs </p>"; 
+        console.log(tweet);
+        t_box.appendChild(tweet); 
+
+        var checkboxes = getCheckBoxes(i); 
+        t_box.appendChild(checkboxes); 
+
+
+    }
+    // console.log(t_box); 
+}
+
+function getCheckBoxes(n) { 
+    var boxes = document.createElement("div");
     
+    for ( link of links ) { 
+        var box = document.createElement("input"); 
+        box.setAttribute("type", "checkbox"); 
+        box.setAttribute("id", link + n); 
+        box.setAttribute("name", "classification" + n); 
+        box.setAttribute("value", link + n); 
+
+        var l = document.createElement("label"); 
+        if(link === "bugs") { 
+            l.setAttribute("for", "bugs/glitches" + n); 
+        }
+        else { 
+            l.setAttribute("for", link + n); 
+        }
+        var a = document.createElement("a"); 
+        a.setAttribute("class", link + "-link"); 
+
+        l.appendChild(a); 
+        box.appendChild(l); 
+        boxes.appendChild(box); 
+    }
+
 }
