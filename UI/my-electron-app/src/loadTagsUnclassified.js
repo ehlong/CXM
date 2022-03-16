937d9985aa946bc572119641633d0b7b3c3178de
Taylor <taylor.c.vo@gmail.com>
2022-03-15 18:01:01 -0700

    Added comments and removed some closing divs

--- UI/my-electron-app/src/loadTagsUnclassified.js
+++ UI/my-electron-app/src/loadTagsUnclassified.js
@@ -7,21 +7,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
 let links = ["positive", "bugs", "security", "store", "wants", "junk"];
 function loadImgs() { 
-
+    //This function is just to insert image based on class.
     for (var link of links) { 
+        //Grab all the elements with -link in them e.g class="positive-link"
         var tweetbox = document.getElementsByClassName(link + "-link"); 
-        // let coloredImg = document.createElement("img"); 
-        // let grayImg = document.createElement("img"); 
-        // coloredImg.setAttribute("class", link + "-color"); 
-        // coloredImg.setAttribute("alt", "check"); 
-        // coloredImg.setAttribute("title", link.toUpperCase()); 
-        // coloredImg.src = "../img/svgs/" + link + "-color.svg"; 
-        
-        // grayImg.setAttribute("class", link + "-gray"); 
-        // grayImg.setAttribute("alt", "check"); 
-        // grayImg.setAttribute("title", link.toUpperCase()); 
-        // grayImg.src = "../img/svgs/" + link + "-gray.svg"; 
+
         for(var i = 0; i < tweetbox.length ; i++) {
+            //Since we have multiple tweets we need to run through all of them and insert the specific class image. In this class we just slapped a innerhtml and copy pasta
             tweetbox[i].innerHTML =   
             "<img class=\"" + link + "-gray\" src=\"../img/svgs/" + link + "-gray.svg\" alt=\"uncheck\" title=\" " + link.toUpperCase() + "\"/>" + "\n" +   
             "<img class=\"" + link + "-color\" src=\"../img/svgs/" + link + "-color.svg\" alt=\"check\" title=\"" + link.toUpperCase() + "\"/>";
@@ -32,20 +24,34 @@ function loadImgs() {
 
 
 function loadTweetBoxes (n) { 
+    //  This function will load tweet boxes in by tweet 
+
+    //  First grab the tag that we are inserting into
     let t_box = document.getElementsByClassName("TweetBox"); 
 
+    //  Create simple loop for how many tweets we want to create a box for. 
     for(var i =1; i <= n; i++) { 
+        // We create the outer div tag and set attributes 
+        // ex <div id ="Tweet#1 class="tweet"> </div>"
         var tweet = document.createElement("div"); 
         tweet.setAttribute("id", "Tweet#" + i); 
         tweet.setAttribute("class", "tweet"); 
+        
+        //  Set the inner html elements, this is when tweet does not load(this should be over written when we grab tweets) 
         tweet.innerHTML = 
         "<h2>TweetID </h2>" + '\n' + 
         "<p> Tweet Stuffs </p>"; 
+
+        //  create another div that will wrap entire thing into a wrapper and insert it
         var wrapper = document.createElement("div"); 
         wrapper.setAttribute("class", "tweet_wrapper");
         wrapper.appendChild(tweet);
+
+        //We create inputs and insert it inside the wrapper div (this should be on same level as the tweet itself.)
         var checkboxes = getCheckBoxes(i); 
         wrapper.appendChild(checkboxes);
+
+        //  Finally insert into html TweetBox from earlier.
         t_box.item(0).appendChild(wrapper);  
         
 
@@ -55,9 +61,11 @@ function loadTweetBoxes (n) {
 }
 
 function getCheckBoxes(n) { 
+    //  Create <div class="inputBox"> </div> 
     var boxes = document.createElement("div");
     boxes.setAttribute("class","inputBox"); 
     
+    //  Now for each class we make a input div with that specific classid and number an exmaple should be <input type="checkbox" id="positive1" name="classification1" value="positive"></input> 
     for ( link of links ) { 
         var box = document.createElement("input"); 
         box.setAttribute("type", "checkbox"); 
@@ -65,6 +73,7 @@ function getCheckBoxes(n) {
         box.setAttribute("name", "classification" + n); 
         box.setAttribute("value", link); 
 
+        //  Create label div <label for="bugs/glitches1"
         var l = document.createElement("label"); 
         if(link === "bugs") { 
             l.setAttribute("for", "bugs/glitches" + n); 
@@ -72,15 +81,20 @@ function getCheckBoxes(n) {
         else { 
             l.setAttribute("for", link + n); 
         }
+        //create a element <a class=positive-link>
         var a = document.createElement("a"); 
         a.setAttribute("class", link + "-link"); 
 
        
-        
+        //  Inside label element we append <a class=positive-link>
         l.appendChild(a);
+        //  Iside input element we append label
         box.appendChild(l);
+
+        //  inside of inputBox element we append input
         boxes.appendChild(box); 
     }
+    //  Return all inputs elements for that specific number. This should return the inputBox that contains everything.
     return boxes; 
 
 }
